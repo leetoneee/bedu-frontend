b121414a014511528b7bd0a32373d1adce713813
chihuy124 <22520577@gm.uit.edu.vn>
2024-11-02 08:58:26 +0700

    define name color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,19 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
+        'primary': '#F9FAFB',
+        'OnPrimary': '#00adef',
+        'Secondary': '#FFFEFA',
+        'OnSecondary': '#FE914C',
+        'Surface': '#f5f6fa',
+        'OnSurface': '#0071F9',
+        'Highlight': '#EBF5FF',
+        'Outline': '#C5C6C9',
+        'OutlineFocus': '#295782',
+        'Error': '#D32F2F',
+        'Success': '#388E3C',
+        'Warning': '#F57C00',
+        'ButtonSI': '#00FF84',
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
