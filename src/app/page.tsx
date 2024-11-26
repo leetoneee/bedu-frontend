fd98ffe70108611665dcffae1c4ce48cb2234e06
chihuy124 <22520577@gm.uit.edu.vn>
2024-11-26 09:50:00 +0700

    fix pull request

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,12 @@ export default function Home() {
   return (
     <div>
       <div>hello</div>
-      <Image src={'icons/logo.svg'} alt="logo" width={200} height={200} />
+      <Image
+        src={'icons/logo.svg'}
+        alt="logo"
+        width={200}
+        height={200}
+      />
     </div>
   );
 }
