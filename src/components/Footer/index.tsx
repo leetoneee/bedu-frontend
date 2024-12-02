a973af2d9fd18b286978ab86ec6c3d019a1d386e
chihuy124 <22520577@gm.uit.edu.vn>
2024-12-03 02:44:10 +0700

    feat footer & homepage

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,78 @@
+'use client';
+
+import React from 'react';
+
+const Footer = () => {
+
+  return (
+    <div className="flex h-60 flex-row bg-white">
+      <div className="ml-12 mt-12 grid grid-cols-4 gap-12">
+        {/* BEDU  */}
+        <div className="col-span-1">
+          <p className="text-2xl font-bold text-on-primary 2xl:text-3xl">
+            BEDU - Become A Better Human
+          </p>
+          <div className="mt-4 flex flex-row items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
+              />
+            </svg>
+            <p className="ml-5 text-xs">buoibuoi@gmail.com</p>
+          </div>
+          <div className="mt-5 flex flex-row items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+              />
+            </svg>
+            <p className="ml-5 text-xs">TP. Hồ Chí Minh</p>
+          </div>
+        </div>
+
+        {/* About BEDU  */}
+        <div className="col-span-1">
+          <p className="text-2xl font-bold text-on-primary 2xl:text-3xl">
+            About BEDU
+          </p>
+          <p className="mt-6 text-xs">About us</p>
+          <p className="mt-3 text-xs">buoibuoi@gmail.com</p>
+        </div>
+
+        {/* BEDU's information  */}
+        <div className="col-span-1">
+          <p className="text-2xl font-bold text-on-primary 2xl:text-3xl">
+            BEDU's Information
+          </p>
+          <p className="mt-6 text-xs">Course List</p>
+          <p className="mt-3 text-xs">Feedback from students</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
