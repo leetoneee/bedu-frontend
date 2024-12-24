23efaf328ec5087dc51afd6bf7d4ec65e56b3c31
Binkeurss <nvbinh271104@gmail.com>
2024-12-24 15:06:26 +0700

    Service: Config API createQuestion, editQuestion, deleteQuestion

---
+++ src/services/questions.service.ts
@@ -0,0 +1,47 @@
+import axios from '@/libs/axiosInstance';
+
+export type CreateQuestionDto = {
+  question: string;
+  totalPoint: number;
+  pointDivision: string;
+  content: string;
+  attach: string;
+  questionType: string;
+  possibleAnswer: string;
+  answer: string;
+  examId?: number[];
+  documentId?: number[];
+};
+
+export const createQuestion = async (data: CreateQuestionDto) => {
+  try {
+    const res = await axios.post(`/question/new`, data);
+    return res.data;
+  } catch (error) {
+    console.log('🚫 ~ createCourse ~ error:', error);
+    throw error;
+  }
+}
+
+
+export type UpdateQuestionDto = Partial<CreateQuestionDto>;
+
+export const editQuestion = async (id: number, data: UpdateQuestionDto) => {
+  try {
+    const res = await axios.patch(`/question/item/${id}`, data);
+    return res.data;
+  } catch (error) {
+    console.log('🚫 ~ editQuestion ~ error:', error);
+    throw error;
+  }
+}
+
+export const deleteQuestion = async (id: number) => {
+  try {
+    const res = await axios.delete(`/question/item/${id}`);
+    return res.data;
+  } catch (error) {
+    console.log('🚫 ~ deleteQuestion ~ error:', error);
+    throw error;
+  }
+}
