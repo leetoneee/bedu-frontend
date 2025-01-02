a1398464be089d608d028f8ee64f06d0dae44103
leetoneee <137870060+leetoneee@users.noreply.github.com>
2025-01-02 22:00:41 +0700

    Tuition (#32)
    * feat(Tuition): done list and delete
    
    * feat(Tuition): done Add and Edit

--- src/app/manager/tuition-fees/page.tsx
+++ src/app/manager/tuition-fees/page.tsx
@@ -1,16 +1,60 @@
+'use client'
+
 import Breadcrumb from '@/components/Breadcrumb';
 import { Crumb } from '@/types';
+import { Divider } from '@nextui-org/react';
+import { useState } from 'react';
+import TuitionHeader from './TuitionHeader';
+import SSPTab from './SSP.Tab';
+import LPTab from './LP.Tab';
 
-export default function SSP() {
+export default function TuitionPage() {
   const crumbs: Crumb[] = [
     {
       label: 'Tuition Fees',
       href: '/manager/tuition-fees'
     }
   ];
+  const [activeTab, setActiveTab] = useState<string>('Self-study Program');
+
   return (
-    <main className="flex flex-col items-center p-3 sm:items-start">
+    <main className="flex flex-col items-center gap-4 p-4 sm:items-start">
       <Breadcrumb crumbs={crumbs} />
+      <Divider />
+      <div className="flex w-full flex-col">
+        <TuitionHeader activeTab={activeTab} setActiveTab={setActiveTab} />
+        <Divider />
+        {activeTab === 'Self-study Program' && <SSPTab />}
+        {activeTab === 'Live Program' && <LPTab />}
+      </div>
+      {/* <AddCourseModal
+      isOpen={isOpen}
+      onOpen={onOpen}
+      onOpenChange={onOpenChange}
+      onClose={onClose}
+      onCreated={handleCreated}
+    />
+    {isOpenE && selectedCourse && (
+      <EditCourseModal
+        isOpen={isOpenE}
+        onOpen={onOpenE}
+        onOpenChange={onOpenChangeE}
+        course={selectedCourse}
+        onClose={handleCloseEditModal}
+        onEdited={handleEdited}
+      />
+    )}
+    {isOpenD && selectedCourse && (
+      <DeleteCourseModal
+        isOpen={isOpenD}
+        onOpen={onOpenD}
+        onOpenChange={onOpenChangeD}
+        onClose={handleCloseDeleteModal}
+        courseId={selectedCourse.id}
+        courseTitle={selectedCourse.title}
+        onDeleted={handleDeleted}
+      />
+    )} */}
     </main>
   );
 }
---
+++ src/types/payment.type.ts
@@ -0,0 +1,13 @@
+import { EClass } from "./class.type";
+import { Program } from "./program.type";
+import { User } from "./user.type";
+
+export type Payment = {
+  id: number;
+  transactionId: string;
+  user: User;
+  program: Program;
+  class: EClass;
+  amount: number;
+  method: string;
+}
