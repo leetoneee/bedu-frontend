67c555222bc140f83b784c929513c59f153418e5
leetoneee <letoan7442@gmail.com>
2025-01-02 20:01:22 +0700

    feat(Tuition): done list and delete

---
+++ src/data/payment.data.ts
@@ -0,0 +1,19 @@
+export const columnsP = [
+  { name: '#', uid: 'id', sortable: true },
+  { name: 'TRANSACTION ID', uid: 'transactionId' },
+  { name: 'USER', uid: 'user', sortable: true },
+  { name: 'PROGRAM', uid: 'program' },
+  { name: 'AMOUNT', uid: 'amount'},
+  { name: 'METHOD', uid: 'method' },
+  { name: 'ACTIONS', uid: 'actions' }
+];
+
+export const columnsL = [
+  { name: '#', uid: 'id', sortable: true },
+  { name: 'TRANSACTION ID', uid: 'transactionId' },
+  { name: 'USER', uid: 'user', sortable: true },
+  { name: 'CLASS', uid: 'class' },
+  { name: 'AMOUNT', uid: 'amount'},
+  { name: 'METHOD', uid: 'method' },
+  { name: 'ACTIONS', uid: 'actions' }
+];
