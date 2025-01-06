150223af0aee70419aa729fe6c396d0de8da7256
Binkeurss <nvbinh271104@gmail.com>
2025-01-06 12:20:58 +0700

    Add context: TypeContext for type(program, eclass)

--- src/contexts/index.ts
+++ src/contexts/index.ts
@@ -1,5 +1,6 @@
 'use client';
 
+import TypeContext from '@/contexts/TypeContext';
 import AppContext from './AppContext';
 import MyProgramContext from './MyProgramContext';
-export { AppContext, MyProgramContext };
+export { AppContext, MyProgramContext, TypeContext };
