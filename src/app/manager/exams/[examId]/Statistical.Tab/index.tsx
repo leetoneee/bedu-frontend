e1e64112a599ea85c2dfd7646bd76e535a422f2f
leetoneee <letoan7442@gmail.com>
2025-01-07 21:18:13 +0700

    hotfix id->examId

--- src/app/manager/exams/[examId]/Statistical.Tab/index.tsx
+++ src/app/manager/exams/[examId]/Statistical.Tab/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 type Props = {
-  id: number;
+  examId: number;
 };
 
-const Statistical = ({ id }: Props) => {
+const Statistical = ({ examId }: Props) => {
   return (
     <div className="flex h-full w-full flex-col gap-2 rounded rounded-t-none border-on-surface/20 bg-white p-5 shadow-sm">
       {/* Code ở đây */}
