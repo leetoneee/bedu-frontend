74a9ad0530491330394806827947651a99d9bae4
Binkeurss <nvbinh271104@gmail.com>
2025-01-08 13:16:41 +0700

    Feat: Update feature Comment

--- src/app/my/courses/[programId]/[lessonId]/Comment.Tab/index.tsx
+++ src/app/my/courses/[programId]/[lessonId]/Comment.Tab/index.tsx
@@ -2,9 +2,14 @@
 
 import { Divider, Input } from '@nextui-org/react';
 import { ButtonSolid, Comment } from '@/components';
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import axios from '@/libs/axiosInstance';
-import useSWR from 'swr';
+import useSWR, { mutate } from 'swr';
+import { CreateCommentDto } from '@/types/comment.type';
+import { AppContext } from '@/contexts';
+import { AuthType } from '@/types';
+import { createComment } from '@/services/comments.service';
+import { toast } from 'react-toastify';
 
 type Props = {
   lessonId: string;
@@ -18,39 +23,26 @@ type CommentProps = {
   hasChildren: boolean;
 };
 
-// const commentData: CommentProps[] = [
-//   {
-//     commentId: 1,
-//     username: 'Nguyen Vu Binh',
-//     content: 'Binh luan 1',
-//     commentTime: new Date('2025-01-07T02:57:36.318Z'),
-//     hasChildren: true
-//   },
-//   {
-//     commentId: 4,
-//     username: 'Le Huu Do',
-//     content: 'Binh luan 2',
-//     commentTime: new Date('2025-01-07T02:57:36.318Z'),
-//     hasChildren: true
-//   },
-//   {
-//     commentId: 12,
-//     username: 'Le Huu Do',
-//     content: 'Binh luan 3',
-//     commentTime: new Date('2025-01-07T02:57:36.318Z'),
-//     hasChildren: true
-//   }
-// ];
 const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
 const CommentTab = ({ lessonId }: Props) => {
   const [comment, setComment] = useState<string>('');
   const [comments, setComments] = useState<CommentProps[]>([]);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [user, setUser] = useState(); // được sử dụng để render tạm thời
 
+  const { auth } = useContext(AppContext) as AuthType;
+  const { data: dataUser } = useSWR(`/users/item/${auth?.id}`, fetcher);
   const { data: dataComments } = useSWR(
     `/comment/parent/${0}?lessonId=${lessonId}`,
     fetcher
   );
+
+  useEffect(() => {
+    if (dataUser) {
+      setUser(dataUser.metadata);
+    }
+  }, [dataUser]);
 
   useEffect(() => {
     console.log('dataComments: ', dataComments);
@@ -59,8 +51,47 @@ const CommentTab = ({ lessonId }: Props) => {
     }
   }, [dataComments?.metadata]);
 
+  const handleComment = async () => {
+    let reply = comment.trim();
+    if (reply === '') {
+      return;
+    }
+
+    const data: CreateCommentDto = {
+      lessonId: Number(lessonId),
+      userId: auth.id,
+      content: comment.trim(),
+      parentCommentId: 0
+    };
+
+    const newComment: CommentProps = {
+      commentId: Date.now(), // Sử dụng tạm ID để hiển thị
+      username: dataUser.metadata.name,
+      content: comment.trim(),
+      commentTime: new Date(),
+      hasChildren: false
+    };
+
+    try {
+      setIsSubmitting(true);
+      const result = await createComment(data);
+      // Hiển thị tạm comment mới
+      setComments((prev) => [...prev, newComment]);
+      mutate(`/comment/parent/${0}?lessonId=${lessonId}`);
+    } catch (error: any) {
+      console.error('🚫 ~ onSubmit ~ Error:', error);
+      toast.error(
+        error.response?.data?.message ||
+          'Failed to create comment. Please try again.'
+      );
+    } finally {
+      setIsSubmitting(false); // Hoàn tất gửi yêu cầu
+      setComment('');
+    }
+  };
+
   return (
-    <div className="flex h-full w-full flex-col gap-2 rounded rounded-t-none bg-white p-5 shadow-sm">
+    <div key={'TabComment'} className="flex h-full w-full flex-col gap-2 rounded rounded-t-none bg-white p-5 shadow-sm">
       <div className="text-2xl font-bold text-on-surface">Comments</div>
       <div className="flex flex-col gap-4 bg-highlight p-4 rounded-md">
         <Input
@@ -74,14 +105,15 @@ const CommentTab = ({ lessonId }: Props) => {
 
         <ButtonSolid
           className="w-[15%] rounded-md bg-outline-focus !text-b-primary"
-          content="Send comment"
+          content={isSubmitting ? 'Submitting' : 'Send comment'}
+          onClick={handleComment}
         />
       </div>
       <div className="text-xl font-bold text-on-surface">{18} comments</div>
       <Divider />
       {comments &&
         comments.map((comment) => (
-          <Comment comment={comment} lessionId={lessonId} />
+          <Comment comment={comment} lessonId={lessonId} />
         ))}
     </div>
   );
